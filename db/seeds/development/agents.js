f03710b01347774b47f2754e864a79b5f63c6112
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-10-07 10:58:38 -0700

    made changes to seeds files

--- db/seeds/development/agents.js
+++ db/seeds/development/agents.js
@@ -1,3 +1,4 @@
+const { commands } = require('../../../build/server/database')
 
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
@@ -5,12 +6,12 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       return Promise.all([
         // Inserts seed entries
-        knex('agents').insert({
+        commands.createAgent({
           firstName: 'Yaseen',
           lastName: 'Hussain',
-          phoneNumber: '5109442727'
+          phoneNumber: '5109442727',
           email: 'Yaseen.uc@gmail.com',
-          imageUlr: 'https://avatars2.githubusercontent.com/u/17223371?v=3&s=400',
+          imageUrl: 'https://avatars2.githubusercontent.com/u/17223371?v=3&s=400',
           password: '123'
           },
           [
@@ -30,14 +31,14 @@ exports.seed = (knex, Promise) => {
             }
           ],
           [1, 2]
-        }),
+        ),
 
-        knex('agents').insert({
+        commands.createAgent({
           firstName: 'Majid',
           lastName: 'Rahimi',
-          phoneNumber: '4152655659'
+          phoneNumber: '4152655659',
           email: 'majid88rahimi@gmail.com',
-          imageUlr: 'https://avatars0.githubusercontent.com/u/7544733?v=3&s=466',
+          imageUrl: 'https://avatars0.githubusercontent.com/u/7544733?v=3&s=466',
           password: '333'
           },
           [
@@ -54,18 +55,20 @@ exports.seed = (knex, Promise) => {
                   "end": "20:00",
                   "start": "08:00"
                }]
-            },
+            }
+          ],
           [3,4,5]
-        }),
+        ),
 
-        knex('agents').insert({
+        commands.createAgent({
           firstName: 'Mike',
           lastName: 'Abelson',
-          phoneNumber: '4157077256'
+          phoneNumber: '4157077256',
           email: 'mikeadelson@yahoo.com',
-          imageUlr: 'http://vignette2.wikia.nocookie.net/spongebob/images/3/33/Patrick_Star.svg/revision/latest?cb=20100724183918',
+          imageUrl: 'http://vignette2.wikia.nocookie.net/spongebob/images/3/33/Patrick_Star.svg/revision/latest?cb=20100724183918',
           password: '777'
-        }
+          },
+          [
             {
                "day": "tuesday",
                "hours": [{
@@ -79,10 +82,11 @@ exports.seed = (knex, Promise) => {
                   "end": "18:00",
                   "start": "06:00"
                }]
-            },
-            [6,7,8]
-        )
-      })
+            }
+          ],
+          [6,7,8]
+        ),
+      
     ]) // closes out line 6
-  } //closes out line 5
-  } // closes out line 2
+  }) //closes out line 5
+} // closes out line 2
